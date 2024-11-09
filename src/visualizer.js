693b79139960725c7c03a2c0e96f7e6b146d2012
coldsn4p <coldsn4p.outward920@passinbox.com>
2024-11-09 23:49:25 +0800

    final touches

--- src/visualizer.js
+++ src/visualizer.js
@@ -39,7 +39,10 @@ const Visualizer = () => {
 
   return (
     <div className="visualizer">
-      <p className="heading">Digital Signal Interactive Visualizer</p>
+      <div className="name">
+        <p className="heading">Digital Signal Interactive Visualizer</p>
+        <p className="">Lucky Jones Uayan</p>
+      </div>
 
       <div className="input-group">
         <input
@@ -57,7 +60,7 @@ const Visualizer = () => {
           }}
           placeholder="Input binary data"
           aria-label="Binary input"
-          maxLength={21}
+          maxLength={20}
         />
 
         <label htmlFor="initialValue">Initial Value</label>
@@ -74,125 +77,131 @@ const Visualizer = () => {
 
       {error && <p className="error">{error}</p>}
 
-      <div className="main">
-        <div className="data">
-          {error.length === 0 &&
-            [...data].map((char, index) => <span key={index}>{char}</span>)}
-        </div>
+      {data.split("").length > 0 ? (
+        <div className="main">
+          <div className="data">
+            {error.length === 0 &&
+              [...data].map((char, index) => <span key={index}>{char}</span>)}
+          </div>
 
-        {techniques.length > 0 &&
-          techniques.map((technique, i) => (
-            <div className="signal" key={i}>
-              <span>{names[i]}</span>
-              <svg
-                width={`${technique.length * 50}`}
-                height="52.5"
-                style={{ border: "1px solid black" }}
-              >
-                {technique.map((_, i) => {
-                  const x = (i + 1) * 50;
-                  return (
-                    <line
-                      key={`col-${i}`}
-                      x1={x}
-                      y1={0}
-                      x2={x}
-                      y2={50}
-                      stroke="black"
-                      strokeWidth="5"
-                      strokeDasharray="5,5"
-                    />
-                  );
-                })}
-                {technique.map((point, i) => {
-                  const x1 = point.x * 50;
-                  const y1 = point.y;
-                  const x2 = (point.x + 1) * 50;
-                  const y2 = point.y;
+          {techniques.length > 0 &&
+            techniques.map((technique, i) => (
+              <div className="signal" key={i}>
+                <span>{names[i]}</span>
+                <svg
+                  width={`${technique.length * 50}`}
+                  height="52.5"
+                  style={{ border: "1px solid black" }}
+                >
+                  {technique.map((_, i) => {
+                    const x = (i + 1) * 50;
+                    return (
+                      <line
+                        key={`col-${i}`}
+                        x1={x}
+                        y1={0}
+                        x2={x}
+                        y2={50}
+                        stroke="black"
+                        strokeWidth="3"
+                        strokeDasharray="2"
+                      />
+                    );
+                  })}
+                  {technique.map((point, i) => {
+                    const x1 = point.x * 50;
+                    const y1 = point.y;
+                    const x2 = (point.x + 1) * 50;
+                    const y2 = point.y;
 
-                  return (
-                    <React.Fragment key={`${x1}-${y1}-${x2}-${y2}`}>
-                      {point.x2 !== undefined && point.y2 !== undefined ? (
-                        <>
-                          {i === 0 && (
+                    return (
+                      <React.Fragment key={`${x1}-${y1}-${x2}-${y2}`}>
+                        {point.x2 !== undefined && point.y2 !== undefined ? (
+                          <>
+                            {i === 0 && (
+                              <line
+                                x1={x1}
+                                y1={point.y2}
+                                x2={x1}
+                                y2={y1}
+                                stroke="black"
+                                strokeWidth="3"
+                              />
+                            )}
+                            {/* 0, 50, 25, 0 */}
+                            {/* 01 */}
+                            <line
+                              x1={x1} //0
+                              y1={y1} //50
+                              x2={point.x2 * 50} //25
+                              y2={y1} //50
+                              stroke="black"
+                              strokeWidth="3"
+                            />
+                            <line
+                              x1={point.x2 * 50}
+                              y1={y2}
+                              x2={point.x2 * 50}
+                              y2={point.y2}
+                              stroke="black"
+                              strokeWidth="3"
+                            />
+                            <line
+                              x1={point.x2 * 50} // 25
+                              y1={point.y2} // 0
+                              x2={x2} // 50
+                              y2={point.y2} // 0
+                              stroke="black"
+                              strokeWidth="3"
+                            />
+                            {technique[i + 1] &&
+                              technique[i + 1].y !== point.y2 && (
+                                <line
+                                  x1={x2}
+                                  y1={point.y2}
+                                  x2={x2}
+                                  y2={technique[i + 1].y}
+                                  stroke="black"
+                                  strokeWidth="3"
+                                />
+                              )}
+                          </>
+                        ) : (
+                          <>
                             <line
                               x1={x1}
-                              y1={point.y2}
-                              x2={x1}
-                              y2={y1}
+                              y1={y1}
+                              x2={x2}
+                              y2={y2}
                               stroke="black"
-                              strokeWidth="5"
+                              strokeWidth="3"
                             />
-                          )}
-                          {/* 0, 50, 25, 0 */}
-                          {/* 01 */}
-                          <line
-                            x1={x1} //0
-                            y1={y1} //50
-                            x2={point.x2 * 50} //25
-                            y2={y1} //50
-                            stroke="black"
-                            strokeWidth="5"
-                          />
-                          <line
-                            x1={point.x2 * 50}
-                            y1={y2}
-                            x2={point.x2 * 50}
-                            y2={point.y2}
-                            stroke="black"
-                            strokeWidth="5"
-                          />
-                          <line
-                            x1={point.x2 * 50} // 25
-                            y1={point.y2} // 0
-                            x2={x2} // 50
-                            y2={point.y2} // 0
-                            stroke="black"
-                            strokeWidth="5"
-                          />
-                          {technique[i + 1] &&
-                            technique[i + 1].y !== point.y2 && (
-                              <line
-                                x1={x2}
-                                y1={point.y2}
-                                x2={x2}
-                                y2={technique[i + 1].y}
-                                stroke="black"
-                                strokeWidth="5"
-                              />
-                            )}
-                        </>
-                      ) : (
-                        <>
-                          <line
-                            x1={x1}
-                            y1={y1}
-                            x2={x2}
-                            y2={y2}
-                            stroke="black"
-                            strokeWidth="5"
-                          />
 
-                          {technique[i + 1] &&
-                            technique[i + 1].y !== point.y && (
-                              <line
-                                x1={x2}
-                                y1={y1}
-                                x2={x2}
-                                y2={technique[i + 1].y}
-                                stroke="black"
-                                strokeWidth="5"
-                              />
-                            )}
-                        </>
-                      )}
-                    </React.Fragment>
-                  );
-                })}
-              </svg>
-            </div>
-          ))}
-      </div>
+                            {technique[i + 1] &&
+                              technique[i + 1].y !== point.y && (
+                                <line
+                                  x1={x2}
+                                  y1={y1}
+                                  x2={x2}
+                                  y2={technique[i + 1].y}
+                                  stroke="black"
+                                  strokeWidth="3"
+                                />
+                              )}
+                          </>
+                        )}
+                      </React.Fragment>
+                    );
+                  })}
+                </svg>
+              </div>
+            ))}
+        </div>
+      ) : (
+        <div className="empty-state">
+          <h1>Empty Data</h1>
+        </div>
+      )}
     </div>
   );
 };
